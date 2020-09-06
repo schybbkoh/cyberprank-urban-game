4b4d7b62a8e3593b009edc91587ff54e270c5e87
schybbkoh <rafal.szybko@gmail.com>
2020-09-06 15:49:05 +0200

    moving time script to a separate file

--- html/results.php
+++ html/results.php
@@ -45,41 +45,17 @@ $dbconn_time_query = 'SELECT extract(epoch from finish_hour) FROM timetable LIMI
 $dbconn_time_query_result = pg_query($dbconn_time_query) or die('Query failed: ' . pg_last_error());
 $game_finish_time = pg_fetch_row($dbconn_time_query_result);
 
+echo '<script type="text/javascript">var js_game_finish_time = ';
+echo $game_finish_time[0];
+echo ';</script>';
+echo '<script type="text/javascript" src="scripts/timer.js"><script>';
+
 pg_free_result($result);
 pg_free_result($dbconn_time_query_result);
 
 pg_close($dbconn);
 ?>
 </h3>
-
-<script>
-// Set the date we're counting down to
-var countDownDate = new Date(<?php echo $game_finish_time[0]?>*1000)
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("timer").innerHTML = "pozostało: " + hours + ":" + minutes + ":" + seconds;
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "KONIEC CZASU GRY";
-  }
-}, 1000);
-</script>
 
 <script>
 function openForm(id) {
