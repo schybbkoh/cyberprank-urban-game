09902eaa098d30d3e12fdb7e478794dd4aa23867
schybbkoh <rafal.szybko@gmail.com>
2020-09-07 23:56:42 +0200

    rolling back on navigation for iphone, screw you apple

--- html/scripts/navigation.js
+++ html/scripts/navigation.js
@@ -4,7 +4,12 @@ function navigate(latitude, longitude) {
 	var link_to_map = '<a href="geo:' + latitude + ',' + longitude + '?q=' + latitude + ',' + longitude + '(zagadka)' + ';">KLIKNIJ TUTAJ, ABY OTWORZYĆ NAWIGACJĘ (ANDROID)</a>';
     } else if(/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
 	// ios
-	var link_to_map = '<a href="comgooglemapsurl://www.google.com/maps/dir/' + latitude + ',' + longitude + ';">KLIKNIJ TUTAJ, ABY OTWORZYĆ NAWIGACJĘ (APPLE)</a>';
+//      one of the following should work, however it seems that there are problems and since I have neither an iDevice at home nor time to debug...
+//      so screw Apple, a regular link would have to suffice
+//      var link_to_map = '<a href="comgooglemapsurl://www.google.com/maps/dir/' + latitude + ',' + longitude + ';">KLIKNIJ TUTAJ, ABY OTWORZYĆ NAWIGACJĘ (APPLE)</a>';
+//      var link_to_map = '<a href="comgooglemaps://daddr=' + latitude + ',' + longitude + '&directionsmode=transit' + ';">KLIKNIJ TUTAJ, ABY OTWORZYĆ NAWIGACJĘ (APPLE)</a>';
+//      var link_to_map = '<a href="https://maps.apple.com/maps/place/' + latitude + ',' + longitude + ';">KLIKNIJ TUTAJ, ABY OTWORZYĆ NAWIGACJĘ (APPLE)</a>';
+        var link_to_map = '<a href="http://www.google.com/maps/place/' + latitude + ',' + longitude + '"' + 'target="_blank">KLIKNIJ TUTAJ, ABY OTWORZYĆ LINK DO MAPY (może nie działać na iPhone)</a>';
     } else {
 	// other mobile or desktop
 	var link_to_map = '<a href="http://www.google.com/maps/place/' + latitude + ',' + longitude + '"' + 'target="_blank">KLIKNIJ TUTAJ, ABY OTWORZYĆ GOOGLE MAPS (PRZEGLĄDARKA)</a>';
