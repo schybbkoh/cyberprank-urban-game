12bef2cdcbd5a3bf43c5a427782a100b5d4fde50
schybbkoh <rafal.szybko@gmail.com>
2020-09-08 21:53:20 +0200

    fixing script description

--- html/scripts/timer.js
+++ html/scripts/timer.js
@@ -15,7 +15,7 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
+  // Display the result in the element with id="timer"
   document.getElementById("timer").innerHTML = "pozostało: " + hours + ":" + minutes + ":" + seconds;
 
   // If the count down is finished, write some text
